0037ca8f190c0857592100e93db086bc48cb8ba2
Planeshifter <pburckhardt@outlook.com>
2014-12-14 02:51:29 -0500

    add compiled code

--- examples/step2_wordVec.js
+++ examples/step2_wordVec.js
@@ -1,1 +1,12 @@
 var w2v = require("../lib/main.js");
+
+w2v.word2vec("../src/phrases.txt","vectors.txt",{
+  cbow:1,
+  size:200,
+  window:8,
+  negative: 25,
+  hs: 0,
+  sample:1e-4,
+  threads:20,
+  iter:15
+});
