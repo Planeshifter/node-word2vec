2a367de4565859cf1b604b6658567560371400d7
Planeshifter <pburckhardt@outlook.com>
2014-12-14 04:20:45 -0500

    linting

--- examples/step3_loadModel.js
+++ examples/step3_loadModel.js
@@ -1,9 +1,11 @@
 var w2v = require("../lib/main.js");
 
 w2v.loadModel("../src/vectors.txt", function(err, model){
-  console.log(model)
-  var dist = model.distance(["switzerland"], 20);
+  console.log(model);
+  var wordVecs = model.getVectors(["king","queen","boy","girl"]);
+  console.log(wordVecs);
+  var dist = model.distance("switzerland", 20);
   console.log(dist);
-  var analogy = model.analogy("woman",["man","king"]);
+  var analogy = model.analogy("woman",["man","king"], 10);
   console.log(analogy);
 });
