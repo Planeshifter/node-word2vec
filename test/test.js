6fc002893d5ac8daac0cc6fe68578defb012cd5f
Planeshifter <pburckhardt@outlook.com>
2015-03-25 22:42:25 -0400

    [UPDATE] style

--- test/test.js
+++ test/test.js
@@ -1,22 +1,24 @@
+'use strict';
+
 var main = require("../lib/main.js");
 
 var chai = require("chai");
 var expect = chai.expect;
 
-describe("loadModel", function(){
-  it("is a callable function", function(){
-    expect(main.loadModel).to.be.a("function");
-  });
+describe("loadModel", function tests() {
+	it("is a callable function", function test() {
+		expect(main.loadModel).to.be.a("function");
+	});
 });
 
-describe("word2vec", function(){
-  it("is a callable function", function(){
-    expect(main.word2vec).to.be.a("function");
-  });
+describe("word2vec", function tests() {
+	it("is a callable function", function test() {
+		expect(main.word2vec).to.be.a("function");
+	});
 });
 
-describe("word2phrase", function(){
-  it("is a callable function", function(){
-    expect(main.word2phrase).to.be.a("function");
-  });
+describe("word2phrase", function tests() {
+	it("is a callable function", function test() {
+		expect(main.word2phrase).to.be.a("function");
+	});
 });
